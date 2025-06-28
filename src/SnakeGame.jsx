dd62ab2513551837ab6ca143982e93b88218fd97
DarAi1 <899988ai@gmail.com>
2025-06-28 04:36:10 +0100

    Update SnakeGame.jsx

--- src/SnakeGame.jsx
+++ src/SnakeGame.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useRef } from "react";
-
 export default function SnakeGame({ onExit }) {
   const canvasRef = useRef(null);
   const [snake, setSnake] = useState([[10, 10]]);
